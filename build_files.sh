00253bf82e1a2375754535864173beb19905cd0c
Shashwat Singh <sedlife@Shashwats-Virtual-Machine.local>
2025-04-08 03:17:22 +0530

    Fix Vercel deployment issues with Python dependencies

--- build_files.sh
+++ build_files.sh
@@ -4,8 +4,11 @@
 # Make build_files.sh executable
 chmod a+x build_files.sh
 
+# Install setuptools first
+pip3 install setuptools==69.0.2
+
 # Install Python dependencies
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 # Collect static files
 python3 manage.py collectstatic --noinput
@@ -15,4 +18,4 @@ mkdir -p staticfiles_build
 mkdir -p staticfiles_build/static
 
 # Copy static files to build directory
-cp -r static/* staticfiles_build/static/ 
+cp -r static/* staticfiles_build/static/ || echo "No static files to copy" 
